1e380a58eb382d39ae0f48c608336f1971e3158a
Your name <lowejosh50@gmail.com>
2018-03-31 15:36:33 +1000

    initial commit

---
+++ src/coll/Movies/Movie.java
@@ -0,0 +1,51 @@
+package coll.Movies;
+
+import java.util.List;
+
+public class Movie {
+	private String title;
+	private int year;
+	private List<Integer> ratings;
+
+	/**
+	 * Constructor. Construct a new movie object.
+	 * @param title
+	 * @param year
+	 */
+	public Movie(String title, int year) {
+		
+	}
+
+	/**
+	 * Rate the movie.
+	 * @param rating
+	 */
+	public void addRating(int rating) {
+
+	}
+
+	/** 
+	 * Get the title of the movie.
+	 * @return the title of the movie as a String.
+	 */
+	public String title() {
+		return null;
+	}
+
+	/** 
+	 * Get the year of the movie.
+	 * @return the year of the movie as an int.
+	 */
+	public int year() {
+		return 0;
+	}
+
+	/** 
+	 * Get the average rating of the movie.
+	 * @return the average rating of the movie as a double.
+	 */
+	public double rating() {
+		return 0;
+	}
+
+}
---
+++ src/patt/ReactorMonitoring/ResearchCentre.java
@@ -0,0 +1,34 @@
+package patt.ReactorMonitoring;
+
+import java.util.Observable;
+
+public class ResearchCentre extends RadiationMonitor {
+
+	/**
+	 * Constructs a ResearchCentre object, which observes reactor radiation readings
+	 * and constantly prints a report with the current moving average of the
+	 * recorded observations.
+	 * 
+	 * @param location.
+	 *            An arbitrary location.
+	 */
+	public ResearchCentre(String location) {
+		super(null);
+	}
+
+	/**
+	 * Updates the monitor with a new observation and prints a report.
+	 */
+	public void update(Observable subject, Object o) {
+
+	}
+
+	/**
+	 * Generates a report of the current moving average, updated by a new
+	 * observation. The moving average is calculated by summing all observations
+	 * made so far, and dividing by the quantity of observations so far.
+	 */
+	public String generateReport() {
+		return null;
+	}
+}
